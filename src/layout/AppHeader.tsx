19d630fa18d5095c7cc87bce36b3bb0c416952a1
Phichayut Rueangsawat <phichayut.r@elegance.co.th>
2025-04-22 10:29:23 +0700

    22/4/68 commit

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -3,7 +3,7 @@ import { ThemeToggleButton } from "@/components/common/ThemeToggleButton";
 import NotificationDropdown from "@/components/header/NotificationDropdown";
 import UserDropdown from "@/components/header/UserDropdown";
 import { useSidebar } from "@/context/SidebarContext";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import React, { useState ,useEffect,useRef} from "react";
 
@@ -84,21 +84,9 @@ const AppHeader: React.FC = () => {
           </button>
 
           <Link href="/" className="lg:hidden">
-            <Image
-              width={154}
-              height={32}
-              className="dark:hidden"
-              src="./images/logo/logo.svg"
-              alt="Logo"
-            />
-            <Image
-              width={154}
-              height={32}
-              className="hidden dark:block"
-              src="./images/logo/logo-dark.svg"
-              alt="Logo"
-            />
-          </Link>
+            <h1>DEMO VERSION</h1>
+            
+                     </Link>
 
           <button
             onClick={toggleApplicationMenu}
