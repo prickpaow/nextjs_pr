3826776e1eebd4728a972e95df8e8b4283bb5346
Phichayut Rueangsawat <phichayut.r@elegance.co.th>
2025-04-22 12:27:14 +0700

    Remove Notify

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -153,7 +153,7 @@ const AppHeader: React.FC = () => {
             <ThemeToggleButton />
             {/* <!-- Dark Mode Toggler --> */}
 
-           <NotificationDropdown /> 
+           {/* <NotificationDropdown />  */}
             {/* <!-- Notification Menu Area --> */}
           </div>
           {/* <!-- User Area --> */}
